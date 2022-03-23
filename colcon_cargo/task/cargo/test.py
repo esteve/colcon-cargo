35cb65276ea824079aa46cbc75421156a8ab9850
Esteve Fernandez <esteve@apache.org>
2022-03-23 16:26:37 +0100

    Generate dummy JUnit-compatible report if test does not produce output

--- colcon_cargo/task/cargo/test.py
+++ colcon_cargo/task/cargo/test.py
@@ -2,6 +2,7 @@
 # Licensed under the Apache License, Version 2.0
 
 import os
+from pathlib import Path
 
 from colcon_cargo.task.cargo import CARGO_EXECUTABLE
 from colcon_core.event.test import TestFailure
@@ -31,7 +32,8 @@ class CargoTestTask(TaskExtensionPoint):
         logger.info(
             "Testing Cargo package in '{args.path}'".format_map(locals()))
 
-        assert os.path.exists(args.build_base)
+        assert os.path.exists(args.build_base), \
+            'Has this package been built before?'
 
         test_results_path = os.path.join(args.build_base, 'test_results')
         os.makedirs(test_results_path, exist_ok=True)
@@ -46,14 +48,35 @@ class CargoTestTask(TaskExtensionPoint):
         if CARGO_EXECUTABLE is None:
             raise RuntimeError("Could not find 'cargo' executable")
 
+        env['CARGO_TARGET_DIR'] = args.build_base
+
+        junit_xml_path = Path(
+            self.context.args.test_result_base
+            if self.context.args.test_result_base
+            else self.context.args.build_base) / 'cargo_test.xml'
+
+        junit_xml_path.parent.mkdir(parents=True, exist_ok=True)
+        junit_xml_path.write_text("""<?xml version="1.0" encoding="UTF-8"?>
+<testsuite name="{self.context.pkg.name}" tests="1" failures="0" time="0" errors="1" skipped="0">
+  <testcase classname="{self.context.pkg.name}" name="pytest.missing_result" time="0">
+    <failure message="The test invocation failed without generating a result file."/>
+  </testcase>
+</testsuite>
+""".format_map(locals()))  # noqa: E501
+
         # invoke cargo test
-        rc = await run(
+        completed = await run(
             self.context,
-            [CARGO_EXECUTABLE, 'test', '-q',
-                '--target-dir', test_results_path],
+            [
+                CARGO_EXECUTABLE,
+                'test',
+                '--all-targets',
+                '-q',
+            ],
             cwd=args.path, env=env)
 
-        if rc.returncode:
+        cmpstdout = completed.stderr
+
+        if completed.returncode:
             self.context.put_event_into_queue(TestFailure(pkg.name))
             # the return code should still be 0
-        return 0
